1581e4046af649eef1c3a13bc7bb4c9184bfc864
Mumuni Tahiru <peptahiru@gmail.com>
2023-03-28 20:20:44 +0000

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const upperDisplay=document.getElementById("upperDisplay");
+const lowerDisplay=document.getElementById("lowerDisplay");
+let operator;
+let num1="";
+let num2="";
+
+//checking my displays
+
+
+
+
+
+
+function add(num1, num2){
+    return num1+num2;
+}
+//create  the function for the subtraction
+function minus(num1, num2){
+    return num1-num2;
+}
+//create the multiplication function
+function times(num1, num2){
+    return num1*num2;
+}
+//create the division function
+function divide(num1, num2){
+    return num1/num2;
+}
+
+
+function operate(num1, num2, operate){
+
+}
